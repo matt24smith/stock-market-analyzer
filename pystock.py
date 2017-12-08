3e2dead35d73f02512ee634cf009e6b99c47e080
Matt Smith <Matthew.Smith10@forces.gc.ca>
2017-12-08 11:41:33 -0400

    generated some preliminary results

--- pystock.py
+++ pystock.py
@@ -32,9 +32,22 @@ import gzip
 import numpy as np
 import psutil
 import hdbscan
+import datetime
 
+epochT2 = datetime.datetime.fromtimestamp(0)
 
 def memory(): return psutil.virtual_memory().percent
+
+
+def epoch3(t):
+    """
+    Faster version to convert from string to epoch int '%Y-%m-%d %H:%M:%S UTC'
+    031157ZJUN15
+    """
+    delta = datetime.datetime.strptime(t, '%Y-%m-%d') - epochT2
+    t = delta.total_seconds()
+    return int(t)
+
 
 def cluster(revenues_assets, netincome_assets, minClustSize, minSamples):
     """
@@ -63,10 +76,10 @@ stocks = {}
 
 # feed me moar data
 for data in datafiles[3:]:
-    if memory() > 75: 
+    if memory() > 75:
         print "\nRunning out of memory! Data parsing has been halted."
         break
-    
+
     print "processing file %s" % data
     with gzip.open(data, 'rb') as f:
         for line in f:
@@ -87,7 +100,7 @@ for data in datafiles[3:]:
             if symbol:
                 splitline = line.split("\t")
                 symbolsdict[splitline[0]] = splitline[1].rstrip()
-            
+
             if price:
                 splitline = line.split(",")
                 symbol = splitline[0]
@@ -108,7 +121,7 @@ for data in datafiles[3:]:
                 pricedatum['volume'] = splitline[6]
                 pricedatum['adj_close'] = splitline[7].rstrip()
                 stocks[symbol]['prices'] = np.append(stocks[symbol]['prices'], pricedatum)
-            
+
             if report:
                 splitline = line.split(",")
                 symbol = splitline[0]
@@ -137,9 +150,8 @@ for data in datafiles[3:]:
 #                reportdatum['cur_assets']
 #                reportdatum['cur_liab']
 #                reportdatum['cash']
-                
+
                 stocks[symbol]['reports'] = np.append(stocks[symbol]['reports'], reportdatum)
-                
 
             count += 1
 
@@ -149,8 +161,8 @@ for data in datafiles[3:]:
 https://www.cs.princeton.edu/sites/default/files/uploads/karina_marvin.pdf
 (Notably, sec. 4.1)
 
-A measure of similarity should be selected to cluster similar candidates 
-together. From the reasoning provided in the paper referred above, the 
+A measure of similarity should be selected to cluster similar candidates
+together. From the reasoning provided in the paper referred above, the
 following parameters should be chosen as clusterable measures.
 
 Ratio of revenues to assets
@@ -160,26 +172,96 @@ Ratio of net income to assets
 
 """
 
+# trim the fat
+for symbol in stocks.keys():
+    try:
+        stocks[symbol]['prices']
+    except KeyError:
+        del stocks[symbol]
+        continue
+
+    if stocks[symbol]['prices'][0]['volume'] == 'volume':
+        del stocks[symbol]
+        continue
+
+    stocks[symbol]['prices'] = np.unique(stocks[symbol]['prices'])
+    stocks[symbol]['prices'] = np.array(sorted(stocks[symbol]['prices'], key=lambda k: epoch3(k['date'])))
+
+# create some sort of measure of similarity
+# in this case, (revenues / assets) and (netincome / assets) are analyzed,
+# along with the rate of change of these variables
 for symbol in stocks.keys():
     times = np.array([])
     revenues_assets = np.array([])
     netincome_assets = np.array([])
-    
+
     if 'reports' in stocks[symbol].keys():
         for report in stocks[symbol]['reports']:
             try:
-                t = report['end_date']
+                t = epoch3(report['end_date'])
                 revOverAsset        = (float(report['revenues'])   / float(report['assets']))
                 netincomeOverAsset  = (float(report['net_income']) / float(report['assets']))
             except ValueError:
                 continue
-        
-            times = np.append(times, t)
-            revenues_assets = np.append(revenues_assets, revOverAsset)
-            netincome_assets = np.append(netincome_assets, netincomeOverAsset)
-            
+
+            if len(times) == 0 or t > times[-1]:
+                times = np.append(times, t)
+                revenues_assets = np.append(revenues_assets, revOverAsset)
+                netincome_assets = np.append(netincome_assets, netincomeOverAsset)
+
         stocks[symbol]['times'] = times
         stocks[symbol]['revenues_assets'] = revenues_assets
         stocks[symbol]['netincome_assets'] = netincome_assets
 
+    try:
+        stocks[symbol]['times'][0]
+        stocks[symbol]['prices']
+    except:
+        del stocks[symbol]
+        continue
+
+    ti = stocks[symbol]['times']
+    ra = stocks[symbol]['revenues_assets']
+    nia = stocks[symbol]['netincome_assets']
+
+    if len(ti) == 1:
+        del stocks[symbol]
+        continue
+
+    # average rate of change in the value of revenues over assets --> deltaRA
+    stocks[symbol]['deltaRA'] = [
+            (ra[i + 1] - ra[i]) * 100000 /
+            ((ti[i+1] - ti[i]) / 60. / 60. / 24.)
+            for i in xrange(len(ra) - 1)]
+
+    stocks[symbol]['avgDeltaRA'] = (sum(stocks[symbol]['deltaRA']) /
+                                    len(stocks[symbol]['deltaRA']))
+
+    #average rate of change in value of netincome over assets --? deltaNIA
+    stocks[symbol]['deltaNIA'] = [
+            (nia[i + 1] - nia[i]) * 1000000000000 /
+            ((ti[i+1] - ti[i]) / 60. / 60. / 24.)
+            for i in xrange(len(nia) - 1)]
+
+    stocks[symbol]['avgDeltaNIA'] = (sum(stocks[symbol]['deltaNIA']) /
+                                     len(stocks[symbol]['deltaNIA']))
+
+
+# time to cluster!
+sortedKeys = np.array(sorted(stocks.keys()))
+avgDeltaRAs = [stocks[key]['avgDeltaRA'] for key in sortedKeys]
+avgDeltaNIAs = [stocks[key]['avgDeltaNIA'] for key in sortedKeys]
+
+clusterer = cluster(avgDeltaRAs, avgDeltaNIAs, minClustSize=5, minSamples=2)
+
+
+# show some results for Apple Inc
+ix = np.where(sortedKeys == 'AAPL')[0][0]
+clusterLabel = clusterer.labels_[ix]
+matchinglabels = np.where(clusterer.labels_ == clusterLabel)[0]
+
+results = sortedKeys[matchinglabels]
+
+for result in results:
+    print result, "\t", symbolsdict[result]
 
